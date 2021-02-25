344e5a325b74b5331232f4be6f6c631cd2391712
Andrew Boulos <boulosandrew1+git@gmail.com>
2021-02-25 02:15:41 -0500

    Backpedal, switching to different method instead of planned one

--- src/main/java/io/github/Bool0s/CompassRightClick/RightClickListener.java
+++ src/main/java/io/github/Bool0s/CompassRightClick/RightClickListener.java
@@ -1,6 +1,7 @@
 package io.github.Bool0s.CompassRightClick;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -12,8 +13,11 @@ public class RightClickListener implements Listener {
 		//Check to see if the player right clicked with a compass in their hand
 		if (event.getMaterial() == Material.COMPASS) {
 			
-			System.out.println(event.getPlayer().getDisplayName() + "clicked with compass!");
-			//Bukkit.getServer().dispatchCommand()
+			//System.out.println(event.getPlayer().getDisplayName() + "clicked with compass!");
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "say " + event.getPlayer().getDisplayName() + " right clicked with a compass");
+			
+			//event.getPlayer().getLocation();
+			
 		}
 	}
 }
